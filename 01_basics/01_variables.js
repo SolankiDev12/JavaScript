87d85203c7d786bac6e9a9c297ded753caae4edf
SolankiDev12 <161466523+SolankiDev12@users.noreply.github.com>
2025-05-29 10:12:20 +0000

    New Files added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 2478
+let accountEmail = 'xyz@gmail.com'
+var accountpwd = 'xyz'
+accountCity = 'Chikhali'
+
+let accountCountry;  //by default undefined
+
+// accountID = 8099 not allowed
+
+accountEmail= 'zyx@gmail.com'
+accountpwd = 'zyx'
+
+console.log(accountID)
+
+console.table([accountID, accountEmail, accountpwd, accountCity,accountCountry])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello")  it wont work here in nodejs but will work in browser
+
+ let number = 911
+ let string = "Dev"
+ let boolean = true
+ let nullval = null
+ let undefinedval //by default undefined
+
+ console.table([number, string, boolean, nullval, undefinedval])
